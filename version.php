973f40245efad541467188272facc0d11ac23c8d
vulopilot <vulopilot@gmail.com>
2026-08-28 14:13:56 +0530

    version change

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2026082800;
-$plugin->requires  = 2025041400;// Require Moodle version 4.4.
+$plugin->requires  = 2025041400;// Require Moodle version 5.0.
 $plugin->component = 'enrol_authorizedotnet';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '2.6.7 (Build: 2026082800)';
